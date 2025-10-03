2db4a5162f741eb2169c31c5b853cdae66603d16
Andrew Kern <awetzelkern@gmail.com>
2025-10-03 16:46:16 +0000

    finally

--- tests/tests.cc
+++ tests/tests.cc
@@ -74,17 +74,3 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
-
-TEST_CASE("Print Incorrect Prompt Ledger", "[ex-4]") {
-  Atm atm;
-  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
-  auto& transactions = atm.GetTransactions();
-  transactions[{12345678, 1234}].push_back(
-      "Withdrawal - Amount: $200.40, Updated Balance: $99.95");
-  transactions[{12345678, 1234}].push_back(
-      "Deposit - Amount: $40000.00, Updated Balance: $40099.90");
-  transactions[{12345678, 1234}].push_back(
-      "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
-  atm.PrintLedger("./prompt.txt", 12345678, 1234);
-  REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
-}
